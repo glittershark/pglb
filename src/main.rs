a3b0df49faa16d94507a1c8a00b9a63931d84fac
Aspen Smith <root@gws.fyi>
2023-10-24 14:13:56 -0400

    Configure tracing subscriber
    Configure a tracing subscriber at startup, with command line options for
    specifying log level (as an EnvFilter), omitting color, and specifying
    log format.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,47 @@
 use std::net::SocketAddr;
+use std::str::FromStr;
 use std::sync::atomic::{AtomicUsize, Ordering};
 
-use clap::Parser;
+use clap::{Parser, ValueEnum};
 use color_eyre::eyre::{Context, Result};
 use futures::future::{try_select, Either};
+use thiserror::Error;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::{self, TcpListener, TcpStream};
 use tracing::{debug, error, info, trace, trace_span, Instrument};
+
+#[derive(Debug, Clone, Copy, ValueEnum)]
+pub enum LogFormat {
+    /// Corresponds to [`tracing_subscriber::fmt::format::Compact`]
+    Compact,
+
+    /// Corresponds to [`tracing_subscriber::fmt::format::Full`]
+    Full,
+
+    /// Corresponds to [`tracing_subscriber::fmt::format::Pretty`]
+    Pretty,
+
+    /// Corresponds to [`tracing_subscriber::fmt::format::Json`]
+    Json,
+}
+
+#[derive(Debug, Error)]
+#[error("Invalid log format '{0}'; expected one of 'compact', 'full', 'pretty', or 'json'")]
+pub struct InvalidLogFormat(String);
+
+impl FromStr for LogFormat {
+    type Err = InvalidLogFormat;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "compact" => Ok(Self::Compact),
+            "full" => Ok(Self::Full),
+            "pretty" => Ok(Self::Pretty),
+            "json" => Ok(Self::Json),
+            _ => Err(InvalidLogFormat(s.to_owned())),
+        }
+    }
+}
 
 /// a better, faster postgres connection pool
 #[derive(Parser)]
@@ -18,6 +53,18 @@ pub struct Options {
     /// TCP address (host:port) of upstream PostgreSQL server(s) to connect to
     #[clap(long, short = 'u', required = true)]
     pub upstream: Vec<String>,
+
+    /// Format to use for log events.
+    #[clap(long, default_value = "full", value_enum)]
+    pub log_format: LogFormat,
+
+    /// Disable colors in all log output
+    #[clap(long)]
+    pub no_color: bool,
+
+    /// Log level filter for spans and events.
+    #[clap(long, default_value = "info")]
+    pub log_level: String,
 }
 
 struct Upstream {
@@ -71,7 +118,7 @@ async fn handle_client(mut downstream: TcpStream, upstream: &'static Upstream) -
 }
 
 pub async fn run(opts: Options) -> Result<()> {
-    let Options { bind, upstream } = opts;
+    let Options { bind, upstream, .. } = opts;
 
     let mut upstream_addrs = Vec::with_capacity(upstream.len());
     for host in upstream {
@@ -105,5 +152,16 @@ pub async fn run(opts: Options) -> Result<()> {
 #[tokio::main]
 async fn main() -> Result<()> {
     color_eyre::install()?;
-    run(Options::parse()).await
+    let opts = Options::parse();
+
+    let s = tracing_subscriber::fmt()
+        .with_env_filter(tracing_subscriber::EnvFilter::new(&opts.log_level));
+    match opts.log_format {
+        LogFormat::Compact => s.compact().init(),
+        LogFormat::Full => s.init(),
+        LogFormat::Pretty => s.pretty().init(),
+        LogFormat::Json => s.json().with_current_span(true).init(),
+    }
+
+    run(opts).await
 }
